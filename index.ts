74945eda3f99e1efa685893d685ec330e46b1fcc
Evert van Brussel <ecvanbrussel@gmail.com>
2023-03-15 17:46:28 +0100

    try with require

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
+import { createRequire } from "https://deno.land/std@0.173.0/node/module.ts"
 // import { Application } from "https://deno.land/x/oak@v12.1.0/mod.ts"
 import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts"
 // import { Telegraf } from "https://esm.sh/telegraf@4.12.2"
 // import { Telegraf } from "npm:telegraf@4.12.2"
 // import { Telegraf } from "https://ga.jspm.io/npm:telegraf@4.12.2/lib/index.js"
-import { Telegraf } from "telegraf"
+
+const require = createRequire(import.meta.url)
+const { Telegraf } = require("telegraf")
+// import { Telegraf } from "telegraf"
 // import * as telegraf from "https://deno.land/x/telegraf@v4.12.2/src/index.ts"
 
 // const { Telegraf } = telegraf;
